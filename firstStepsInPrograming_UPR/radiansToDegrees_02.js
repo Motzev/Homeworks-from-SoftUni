1daa93add18bec7009d93f2cdf823f2227f080f8
Motzev <107246853+Motzev@users.noreply.github.com>
2022-06-10 14:30:01 +0300

    Delete firstStepsInPrograming_UPR directory

--- firstStepsInPrograming_UPR/radiansToDegrees_02.js
+++
@@ -1,9 +0,0 @@
-function radiansToDegrees_02(input){
-    
-    let radians = Number (input[0]);
-    let degrees = Number (radians * (180 / Math.PI));
-
-    console.log(degrees);
-}
-
-radiansToDegrees_02(["6.2832"]);
